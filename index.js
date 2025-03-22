8e2072d2fee50a6b21f1a8c27d1999a959121772
Ashvin Kanzariya <ashvin@arhantsolutions.com>
2025-03-22 12:03:40 +0530

    String

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ let Ashvin = NaN
 console.log(Ashvin);
 console.log(typeof(Ashvin));
 let ashvinnum=Number(Ashvin)
-console.log(ashvinnum)
+console.log(ashvinnum);
+
+let name="ashvin"
+let age=21;
+
+
+console.log(`my name is ${name} and my age is ${age}`);
+
+const gameName=new String("ashvin12")
+
+console.log(gameName)
